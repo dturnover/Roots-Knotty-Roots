a34148622c68d07228e6b188e072413f408475eb
Desmond Turner <33734477+dturnover@users.noreply.github.com>
2025-11-30 03:25:24 -0800

    Update vite.config.js

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Roots-Knotty-Roots/',   // repo name with slashes
   plugins: [react()],
 })
